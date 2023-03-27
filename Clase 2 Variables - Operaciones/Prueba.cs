a25ae30221a203f0f929d7d93a81fb8c065bc7bf
Ezequiel Bosco <ezequiel_bosco@hotmail.com>
2023-03-27 18:47:49 -0300

    Creo repositorio y agrego ejercicios clase 2

---
+++ Clase 2 Variables - Operaciones/Prueba.cs
@@ -0,0 +1,26 @@
+﻿namespace Ejercicios
+{
+  class Program
+  {
+  static void Main(string[] args)
+  {
+    
+    // int total;
+    // int valor1, valor2;
+    // String nombre = "Ezequiel";
+    // double altura = 25.5;
+    // float ancho;
+    // String apellido = Console.ReadLine();
+
+    
+    // ancho = 2.7f;
+    // valor1 = 2;
+    // valor2 = 4;
+    // total = valor1 * valor2;
+
+    // Console.WriteLine(nombre + " " + apellido + " Altura: " + altura + " Edad: " + ancho + " El resultado es: {0} ",total);
+
+    // 13. Pedir el nombre, la edad e informar por pantalla “En 10 años (nombre) va a tener X años”.
+  }
+  }
+}
